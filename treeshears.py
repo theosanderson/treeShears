25c69ccf66c9a8cc248de6b28aadf7a2845df40a
Theo Sanderson <theo@theo.io>
2022-05-03 16:24:29 +0100

    Update treeshears.py

--- treeshears.py
+++ treeshears.py
@@ -51,6 +51,7 @@ to_remove_file.close()
 
 #split args.output into directory and filename
 output_dir, output_file = os.path.split(args.output)
+print(f"O{output_dir}O")
 output_dir = output_dir if output_dir.trim() != "" else "."
 
 command = f"matUtils extract -i {treefile} -p -s {temp_dir}/to_remove.txt -d {output_dir} -o {output_file} -O"
